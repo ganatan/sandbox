cc3a8be9529091f8acaed05512cab4d56fd5ed3b
ganatan <dannyganatan@gmail.com>
2025-04-27 16:56:45 +0200

    Improve prod & development .env on build

--- backend-javascript-cjs/tools/scripts/generate-project-structure.js
+++ backend-javascript-cjs/tools/scripts/generate-project-structure.js
@@ -35,6 +35,7 @@ function generateStructureForFolders(folders) {
       fullStructure += `\n${folder} directory does not exist.\n`;
     }
   }
+
   return fullStructure;
 }
 
@@ -42,6 +43,7 @@ function generateStructureForRoot() {
   const rootPath = path.join(__dirname, '..', '..');
   let structure = '\nStructure of project root:\n';
   structure += getDirectoryStructure(rootPath, 0);
+
   return structure;
 }
 
@@ -56,50 +58,3 @@ module.exports = {
   generateStructureForFolders,
   generateStructureForRoot,
 };
-
-
-// 'use strict';
-
-// const fs = require('fs');
-// const path = require('path');
-
-// function getDirectoryStructure(dirPath, level = 0) {
-//   const files = fs.readdirSync(dirPath);
-//   let structure = '';
-
-//   for (const file of files) {
-//     const fullPath = path.join(dirPath, file);
-//     const isDirectory = fs.lstatSync(fullPath).isDirectory();
-//     structure += `${'  '.repeat(level)}|-- ${file}\n`;
-//     if (isDirectory) {
-//       structure += getDirectoryStructure(fullPath, level + 1);
-//     }
-//   }
-
-//   return structure;
-// }
-
-// function generateStructureForFolders(folders) {
-//   let fullStructure = '';
-//   for (const folder of folders) {
-//     const folderPath = path.join(__dirname, '..', '..', folder);
-//     if (fs.existsSync(folderPath)) {
-//       fullStructure += `\nStructure of ${folder}:\n`;
-//       fullStructure += getDirectoryStructure(folderPath);
-//     } else {
-//       fullStructure += `\n${folder} directory does not exist.\n`;
-//     }
-//   }
-
-//   return fullStructure;
-// }
-
-// const foldersToInspect = ['src', '__tests__', 'tools'];
-// const projectStructure = generateStructureForFolders(foldersToInspect);
-
-// console.log(projectStructure);
-
-// module.exports = {
-//   getDirectoryStructure,
-//   generateStructureForFolders,
-// };
