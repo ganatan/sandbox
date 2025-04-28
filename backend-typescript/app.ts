5e949aceeeed5dd79677d1e770da3025a96f4fd9
ganatan <dannyganatan@gmail.com>
2025-04-28 09:50:24 +0200

    Add lint on backend-typescript

--- backend-typescript/app.ts
+++ backend-typescript/app.ts
@@ -1,7 +1,7 @@
-import express, { Request, Response } from 'express'
+import express, { Request, Response } from 'express';
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 const persons = [
   { id: 1, name: 'Steven Spielberg' },
@@ -10,13 +10,13 @@ const persons = [
   { id: 4, name: 'Martin Scorsese' },
   { id: 5, name: 'Alfred Hitchcock' },
   { id: 6, name: 'Stanley Kubrick' },
-  { id: 7, name: 'James Cameron' }
-]
+  { id: 7, name: 'James Cameron' },
+];
 
 app.get('/persons', (req: Request, res: Response) => {
-  res.json(persons)
-})
+  res.json(persons);
+});
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`)
-})
+  console.log(`Server is running on http://localhost:${port}`);
+});
