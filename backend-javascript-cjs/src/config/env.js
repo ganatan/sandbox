eb4bc0604e6ac290d6da1795af035b91defc92ff
ganatan <dannyganatan@gmail.com>
2025-04-28 18:33:05 +0200

    add .env.test on backend-javascript-cjs

--- backend-javascript-cjs/src/config/env.js
+++ backend-javascript-cjs/src/config/env.js
@@ -4,8 +4,20 @@ const path = require('path');
 const dotenv = require('dotenv');
 
 const nodeEnv = process.env.NODE_ENV || 'development';
-const envFile = process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development';
+
+let envFile = '.env.development';
+
+if (nodeEnv === 'production') {
+  envFile = '.env.production';
+} else if (nodeEnv === 'test') {
+  envFile = '.env.test';
+}
+
 dotenv.config({ path: path.resolve(process.cwd(), envFile) });
+
+console.log(`0000000001: ${process.env.NODE_ENV}`);
+console.log(`0000000002: ${nodeEnv}`);
+console.log(`0000000003: ${envFile}`);
 
 const env = {
   nodeEnv: nodeEnv,
