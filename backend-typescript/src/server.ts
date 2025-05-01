25929ae5698264882c6998816232b74298821d4d
ganatan <dannyganatan@gmail.com>
2025-05-01 16:11:06 +0200

    Add ports test on backend-typescript

--- backend-typescript/src/server.ts
+++ backend-typescript/src/server.ts
@@ -3,7 +3,17 @@ import appConfig from './config/app.config';
 import { Server } from 'http';
 
 const server: Server = app.listen(appConfig.app.port, () => {
-  console.log(`API listening on http://localhost:${appConfig.app.port}`);
+  console.log(`✅ API listening on http://localhost:${appConfig.app.port}`);
+});
+
+server.on('error', (error: NodeJS.ErrnoException) => {
+  if (error.code === 'EADDRINUSE') {
+    console.error(`❌ Port ${appConfig.app.port} is already in use.`);
+    process.exit(1);
+  } else {
+    console.error('❌ Unexpected server error:', error);
+    process.exit(1);
+  }
 });
 
 export default server;
