bc73e569c4380323a15516af1b6abb98397f0d34
ganatan <dannyganatan@gmail.com>
2025-07-02 10:27:13 +0200

    Improve websocket server on electron-starter

--- electron-starter/electron/main.js
+++ electron-starter/electron/main.js
@@ -15,21 +15,3 @@ const createWindow = () => {
 app.whenReady().then(() => {
   createWindow()
 })
-
-
-// const { app, BrowserWindow } = require('electron')
-
-// const createWindow = () => {
-//   console.log('00000000001:main');
-//   const win = new BrowserWindow({
-//     width: 800,
-//     height: 600
-//   })
-//   console.log('00000000002:main');
-//   win.loadFile('index.html')
-// }
-
-// app.whenReady().then(() => {
-//   console.log('00000000003:main');
-//   createWindow()
-// })
