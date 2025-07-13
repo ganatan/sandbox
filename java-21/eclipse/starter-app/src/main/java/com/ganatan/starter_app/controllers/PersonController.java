dd2733fdb929e649a6f5e4ee79e19a2bfe17e6cf
ganatan <dannyganatan@gmail.com>
2025-07-13 13:33:16 +0200

    Improve docs java

--- java-21/eclipse/starter-app/src/main/java/com/ganatan/starter_app/controllers/PersonController.java
+++ java-21/eclipse/starter-app/src/main/java/com/ganatan/starter_app/controllers/PersonController.java
@@ -2,24 +2,34 @@ package com.ganatan.starter_app.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
-public class ApiController {
+public class PersonController {
 
 	private static int getSum(int val1, int val2) {
 		int result = val1 + val2;
 		return result;
 	}
 
-	@GetMapping("/api")
-	public String api() {
+	@GetMapping("/persons")
+	public List<String>api() {
 		int value1 = 10;
 		int value2 = 30;
-		int total = ApiController.getSum(value1, value2);
+		int total = PersonController.getSum(value1, value2);
 
 		System.out.println("000000000002");
 		String response = String.valueOf(total);
 
-		return "starter-app" + response;
+		List<String> names = new ArrayList<>();
+		names.add("James Cameron");
+		names.add("Christopher Nolan");
+
+		System.out.println("000000000002:" + names);
+
+//		return "starter-app" + response;
+		return names;
+
 	}
 }
