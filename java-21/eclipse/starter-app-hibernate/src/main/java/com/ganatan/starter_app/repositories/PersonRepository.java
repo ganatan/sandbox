2098edbdff6ed892e9ea9ee0ced1e92aae4a2b94
ganatan <dannyganatan@gmail.com>
2025-07-15 06:56:56 +0200

    remove useless repo

--- java-21/eclipse/starter-app-hibernate/src/main/java/com/ganatan/starter_app/repositories/PersonRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ganatan.starter_app.repositories;
-
-import com.ganatan.starter_app.entities.Person;
-import java.util.List;
-
-public interface PersonRepository {
-    List<Person> findAll();
-}
