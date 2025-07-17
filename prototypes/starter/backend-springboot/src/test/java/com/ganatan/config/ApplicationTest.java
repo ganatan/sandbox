c1aa054a646c8b6b744c9bcb5b2cdabb8617682f
ganatan <dannyganatan@gmail.com>
2025-07-17 12:38:34 +0200

    improve tests on backend-springboot

--- prototypes/starter/backend-springboot/src/test/java/com/ganatan/config/ApplicationTest.java
+++ prototypes/starter/backend-springboot/src/test/java/com/ganatan/config/ApplicationTest.java
@@ -1,15 +1,12 @@
 package com.ganatan.config;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-@SpringBootTest
 public class ApplicationTest {
 
     @Test
-    void applicationContextLoads() {
-        assertTrue(true);
+    void basicTest() {
+        assertThat(1 + 1).isEqualTo(2);
     }
 }
