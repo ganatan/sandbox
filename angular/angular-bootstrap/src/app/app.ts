18fa9435cc41bc9dd60ea61e5fb9a0c96e5c7229
ganatan <dannyganatan@gmail.com>
2025-09-23 10:42:45 +0200

    Add eslint on angular-bootstrap & gitlab-ci

--- angular/angular-bootstrap/src/app/app.ts
+++ angular/angular-bootstrap/src/app/app.ts
@@ -9,4 +9,7 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('angular-starter');
+  // constructor() {
+  //   console.log('00000000001');
+  // }
 }
